eef9c28c772242cc5438ef2fbe83655a2a8d75d1
snov <snov.contact@gmail.com>
2017-02-13 21:02:17 +0700

    eslint inline options example

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 const f = (x) => x + '!';
 
+/* eslint-disable no-unused-vars */
 const b = 1;
+/* eslint-enable no-unused-vars */
 
 console.log(`I am linted by eslint and transpiled by babel ${f(', cool')}`);
